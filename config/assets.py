18b8a970e17d5bf9001352b06fe31f8826369682
Roos de Kok <roos.dekok@quintel.com>
2020-07-27 16:43:09 +0200

    Update config files

--- config/assets.py
+++ config/assets.py
@@ -48,28 +48,81 @@ assets = {
         'AggregatedBuilding': [
             {
                 'attribute': 'numberOfBuildings',
-                'input': None,
-                'factor': None,
-                'aggregation': 'sum'
-            }
-        ],
-        'HeatPump': [
-            {
-                'attribute': None,
-                'input': 'households_heater_heatpump_air_water_electricity_share',
+                'dependent_on': {'distribution': 'buildingTypeDistribution'}, # TODO
+                'input': 'households_number_of_residences',
                 'factor': 1,
                 'aggregation': 'sum'
-            }
-        ],
-        'HeatNetwork': [
+            },
             {
-                'attribute': None,
-                'input': 'households_heater_district_heating_steam_hot_water_share',
-                'factor': 1,
-                'aggregation': 'sum'
-            }
+                'distribution': 'buildingTypeDistribution', # TODO
+                'inputs': [
+                    'households_number_of_residences',
+                ]
+            },
+            {
+                'distribution': 'energyLabelDistribution',
+                'inputs': [
+                    'households_insulation_level_apartments',
+                    'households_insulation_level_corner_houses',
+                    'households_insulation_level_detached_houses',
+                    'households_insulation_level_semi_detached_houses',
+                    'households_insulation_level_terraced_houses',
+                    'buildings_insulation_level'
+                ],
+            },
+
         ],
     },
 
     'buildings': {},
 }
+
+heating_technologies = {
+    # Elektrische luchtwarmtepomp
+    'HeatPump': [
+        {
+            'dependent_on': {'asset': 'AggregatedBuilding'},
+            'attribute': 'source',
+            'value': 'AIR',
+            'input': 'households_heater_heatpump_air_water_electricity_share',
+        }
+    ],
+    # Elektrische bodemwarmtepomp
+    'HeatPump': [
+        {
+            'dependent_on': {'asset': 'AggregatedBuilding'},
+            'attribute': 'source',
+            'value': 'AQUIFER',
+            'input': 'households_heater_heatpump_ground_water_electricity_share',
+        }
+    ],
+    # Elektrische bodemwarmtepomp
+    'HeatPump': [
+        {
+            'dependent_on': {'asset': 'AggregatedBuilding'},
+            'attribute': 'source',
+            'value': 'SUB_SURFACE',
+            'input': 'households_heater_heatpump_ground_water_electricity_share',
+        }
+    ],
+    # Hybride warmtepomp op methaan
+    'HeatNetwork': [
+        {
+            'dependent_on': {'asset': 'AggregatedBuilding'},
+            'attribute': 'additionalHeatingSourceType',
+            'value': 'GAS',
+            'input': 'households_heater_hybrid_heatpump_air_water_electricity_share',
+        }
+    ],
+    # Hybride warmtepomp op waterstof
+    'HeatNetwork': [
+        {
+            'dependent_on': {'asset': 'AggregatedBuilding'},
+            'attribute': 'additionalHeatingSourceType',
+            'value': 'HYDROGEN',
+            'input': 'households_heater_hybrid_hydrogen_heatpump_air_water_electricity_share',
+        }
+    ],
+},
+
+# In case of an AggregatedBuilding, loop over the following distributions
