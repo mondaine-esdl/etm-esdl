6f5b6edae7b6a50c9032509c86b0bcd0d08acc6e
Anthony Williams <hi@antw.dev>
2022-03-17 11:48:05 +0000

    Respect commit hash given to fetch_esdl_resource
    - Updates the fetch_esdl_resource task to use the commit hash given.
      `sys.argv` always has length >= 1, since the first argument is the
      name of the script, and the following arguments are those given by the
      user.
    
    - Update the commit hash in Pipfile to the last version with which the
      "latest" image was successfully built.

--- lib/tasks/fetch_esdl_resource.py
+++ lib/tasks/fetch_esdl_resource.py
@@ -10,10 +10,10 @@ VENDOR_FOLDER_PATH = 'vendor/esdl/'
 ECORE_FILE_PATH = VENDOR_FOLDER_PATH + 'esdl.ecore'
 
 if __name__ == "__main__":
-    commit_hash = sys.argv[0] if len(sys.argv) == 1 else ''
+    commit_hash = sys.argv[1] if len(sys.argv) == 2 else ''
 
     if commit_hash:
-        github_url = f'https://raw.githubusercontent.com/EnergyTransition/ESDL/#{commit_hash}/esdl/model/esdl.ecore'
+        github_url = f'https://raw.githubusercontent.com/EnergyTransition/ESDL/{commit_hash}/esdl/model/esdl.ecore'
     else:
         github_url = 'https://raw.githubusercontent.com/EnergyTransition/ESDL/master/esdl/model/esdl.ecore'
 
@@ -25,4 +25,4 @@ if __name__ == "__main__":
         with open(ECORE_FILE_PATH, 'wb') as file:
             file.write(result.content)
     else:
-        raise ValueError(f'Could not fetch ESDL ecore resource from github #{commit_hash}')
+        raise ValueError(f'Could not fetch ESDL ecore resource from github {commit_hash}')
