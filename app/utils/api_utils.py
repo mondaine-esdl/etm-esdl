486803048ce69a36e32c2b80bf54d101980a4325
Nora Schinkel <ncschinkel@gmail.com>
2022-11-30 13:14:50 +0100

    Fix ETEngine errors not displaying correctly

--- app/utils/api_utils.py
+++ app/utils/api_utils.py
@@ -8,10 +8,9 @@ def fail_with(result):
     if not len(result.errors) > 0:
         raise ETMParseError('Something went wrong')
 
-    if result.errors[0] == 'ETEngine returned a 429' or result.errors[0] == 'Too many requests':
-        raise ETMParseError.with_humanized_message('too_many_requests', status_code=429)
-
     if isinstance(result.errors, list):
+        if result.errors[0] == 'ETEngine returned a 429' or result.errors[0] == 'Too many requests':
+            raise ETMParseError.with_humanized_message('too_many_requests', status_code=429)
         raise ETMParseError.with_humanized_message(result.errors)
 
     message = ', '.join([f"{key} {', '.join(value)}" for key, value in result.errors.items()])
