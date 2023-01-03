dbd530ae69b939b03254ac35a8c02b7f296e402b
redekok <roos.dekok@quintel.com>
2023-01-03 14:44:02 +0100

    Expand flexibility parser with functions to update the FLH values in the ESDL

--- app/models/parsers/flexibility.py
+++ app/models/parsers/flexibility.py
@@ -2,6 +2,7 @@
 Parser for flexibility assets (electrolyzers)
 """
 
+from app.services.query_scenario import QueryScenario
 from .parser import CapacityParser
 
 class FlexibilityParser(CapacityParser):
@@ -13,6 +14,7 @@ class FlexibilityParser(CapacityParser):
 
     def __init__(self, energy_system, props, *args, **kwargs):
         super().__init__(energy_system, props, *args, **kwargs)
+        self.full_load_hours = 0.
         self.power = 0. # is this line of code redundant?
 
 
@@ -24,11 +26,22 @@ class FlexibilityParser(CapacityParser):
         """
 
         self.power = sum((self.__value_of(asset, 'power') for asset in self.asset_generator))
-        self.inputs[self.props['input']] = self.power
+        self.inputs[self.props['attr_set']['power']['input']] = self.power
+
+
+    def update(self, scenario_id):
+        """
+        Find the flexibility asset and update the number of full load hours
+        with the ETM value
+
+        Sets self.full_load_hours
+        """
+
+        self.update_flh()
 
 
     def __value_of(self, asset, key):
-        '''
+        """
         Returns the value multiplied with the config factor for the key
 
         Params:
@@ -37,7 +50,42 @@ class FlexibilityParser(CapacityParser):
 
         Returns:
             float
-        '''
-        if key!=self.props['attribute']: return 0.0
+        """
 
-        return getattr(asset, key) * self.props['factor']
+        if not key in ['power', 'fullLoadHours']: return 0.0
+
+        return getattr(asset, key) * self.props['attr_set'][key]['factor']
+
+
+    def __update_flh(self, scenario_id):
+        """
+        For each instance in the list of assets of this type of supply, update
+        the number of full load hours to the ETM value.
+        """
+
+        self.full_load_hours = (
+            self.query_scenario(scenario_id, self.props['attr_set']['full_load_hours']) /
+            self.props['attr_set']['fullLoadHours']['factor']
+        )
+
+        for asset in self.asset_generator:
+            asset.fullLoadHours = self.full_load_hours
+
+
+    def __query_scenario(self, scenario_id, prop):
+        """
+        Query the ETM scenario for the value to set the given prop to
+
+        Params:
+            scenario_id (str): e.g. '123456'
+            prop (str): e.g. 'fullLoadHours'
+        """
+
+        query_result = QueryScenario.execute(scenario_id, prop['gquery'])
+
+        if query_result.successful:
+            return query_result.value[prop['gquery']]['future'] / prop ['factor']
+
+        raise ETMParseError(
+            f"We currently do not support the ETM gquery listed in the config: {prop['gquery']}"
+        )
