404550a855a8166a0b3da4b10bff6083a50e747e
noracato <ncschinkel@gmail.com>
2023-02-20 12:22:16 +0100

    Do not deepcopy area but change reference instead

--- app/models/energy_system/energy_system_handler/top_level_area.py
+++ app/models/energy_system/energy_system_handler/top_level_area.py
@@ -1,6 +1,5 @@
 #  Methods to check for, and insert a top level area into the ESDL
 import esdl
-import copy
 
 def is_top_area(self, area_name):
     '''Returns True if the area is the top area of the ESDL'''
@@ -11,6 +10,5 @@ def add_top_level_area(self, area_name):
     instance = self.get_energy_system().instance[0]
 
     top_area = esdl.Area(name=area_name)
-    old_area = copy.deepcopy(self.area())
+    self.area().containingArea = top_area
     instance.area = top_area
-    old_area.containingArea = top_area
