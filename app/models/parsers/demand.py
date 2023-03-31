304d3ee0a8e1d46fbf5584e7025c01e63820788d
redekok <roos.dekok@quintel.com>
2023-03-31 15:35:49 +0200

    Dynamically set number of FLH for mobility demand assets based on ETM query values

--- app/models/parsers/demand.py
+++ app/models/parsers/demand.py
@@ -41,34 +41,25 @@ class MobilityDemandParser(CapacityParser):
 
     def update(self, scenario_id):
         """
-        Find the demand asset and update the value of the (input) capacity
-        with the ETM value
+        Find the demand asset and update the value of the (input) capacity and
+        the full load hours with the ETM value.
 
-        Sets self.power and self.fullLoadHours
-        """
-
-        self.__update_power(scenario_id)
-
-
-    def __update_power(self, scenario_id):
-        """
         For the first instance in the list of assets of this type of demand, update
         the power to the ETM value and (for now, statically) set the number of 
         full load hours.
 
         A warning is provided if there are more instances of this asset.
+
+        Sets self.power and self.fullLoadHours
         """
         asset = self.__next_asset()
 
-        # TODO: As a QUICK & DIRTY FIX the full load hours for this type of consumers 
-        # (the different types of mobility demand) are statically set to 8760.
-        # The ETM doesn't provide a gquery returning this value so, ideally, these
-        # should be derived from the profile attached to the asset. Since these
-        # consumers have flat curves we can assume the number of full load hours
-        # to be 8760.
         if asset:
-            asset.power = self.query_scenario(scenario_id)
-            asset.fullLoadHours = 8760.
+            self.power = self.query_scenario(scenario_id, self.props['attr_set']['power'])
+            self.full_load_hours = self.query_scenario(scenario_id, self.props['attr_set']['fullLoadHours'])
+
+        self.__update_power(asset)
+        self.__update_flh(asset)
 
         # TODO: provide warning if there's more than 1 asset in the asset_generator
         # How do we want to communicate this to the user? An extra part of the API
@@ -78,6 +69,22 @@ class MobilityDemandParser(CapacityParser):
         #     print('A warning')
 
 
+    def __update_power(self, asset):
+        """
+        Sets the power of the asset to the ETM value (self.power)
+        """
+        asset.power = self.power
+
+
+    def __update_flh(self, asset):
+        """
+        Sets the full load hours of the asset to the ETM value 
+        (self.full_load_hours)
+        """
+        # ESDL expects the FLH to be an integer value
+        asset.fullLoadHours = int(self.full_load_hours)
+
+
     def __next_asset(self):
         try:
             return next(self.asset_generator)
@@ -85,7 +92,7 @@ class MobilityDemandParser(CapacityParser):
             return
 
 
-    def query_scenario(self, scenario_id):
+    def query_scenario(self, scenario_id, prop):
         """
         Query the ETM scenario for the value to set the given prop to
 
@@ -94,11 +101,11 @@ class MobilityDemandParser(CapacityParser):
             prop (str): e.g. 'fullLoadHours'
         """
 
-        query_result = QueryScenario.execute(scenario_id, self.props['gquery'])
+        query_result = QueryScenario.execute(scenario_id, prop['gquery'])
 
         if query_result.successful:
-            return query_result.value[self.props['gquery']]['future'] / self.props['factor']
+            return query_result.value[prop['gquery']]['future'] / prop ['factor']
 
         raise ETMParseError(
-            f"We currently do not support the ETM gquery listed in the config: {self.props['gquery']}"
+            f"We currently do not support the ETM gquery listed in the config: {prop['gquery']}"
         )
