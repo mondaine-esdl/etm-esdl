e1e69083cf4ac7b3aed5c49271f6c578d682dc07
redekok <roos.dekok@quintel.com>
2023-05-03 13:54:18 +0200

    Refactor the API calls to get the query values in the right format

--- app/services/query_scenario.py
+++ app/services/query_scenario.py
@@ -9,9 +9,8 @@ class QueryScenario(EtengineService):
     Query a scenario with supplied gqueries (one or more).
     Returns a ServiceResult with a dict containing the gquery results as value.
     """
-    def __call__(self, *gqueries, detailed=False):
-        # Do we need UserValues here? Or can detailed param be omitted?
-        data = {"gqueries": list(gqueries), "detailed": True}
+    def __call__(self, *gqueries):
+        data = {"gqueries": list(gqueries)}
 
         response = self.session.put(
             '/scenarios/' + str(self.scenario_id),
@@ -19,16 +18,15 @@ class QueryScenario(EtengineService):
             headers={'Connection': 'close'}
         )
 
-        return self.__handle_response(response, detailed)
+        return self.__handle_response(response)
 
-    def __handle_response(self, response, detailed):
+    def __handle_response(self, response):
         '''
         Returns a service result, by which we can check later if it's a success or not
         '''
         if response.ok:
             value = response.json()['gqueries']
-            if detailed:
-                value.update(response.json()['scenario'])
+            value.update(response.json()['scenario'])
 
             return ServiceResult.success(value)
         try:
